8e93c2605db0b3349c9d295fdd0c89bfa873673c
Andrei Yangabishev <waffleboot@gmail.com>
2021-06-09 03:52:20 +0300

    add run function

--- grabxkcd/grabxkcd.go
+++ grabxkcd/grabxkcd.go
@@ -1,6 +1,7 @@
 package grabxkcd
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -11,11 +12,19 @@ func (app *appEnv) fromArgs([]string) error {
 	return nil
 }
 
+func (app *appEnv) run() error {
+	return nil
+}
+
 func CLI(args []string) int {
 	var app appEnv
 	err := app.fromArgs(args)
 	if err != nil {
 		return 2
+	}
+	if err = app.run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Runtime error: %v\n", err)
+		return 1
 	}
 	return 0
 }
