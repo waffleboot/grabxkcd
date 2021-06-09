4c0ff9e4c53f03ac7f0d3cb44b0d47176356aee6
Andrei Yangabishev <waffleboot@gmail.com>
2021-06-09 03:50:03 +0300

    parse args

--- grabxkcd/grabxkcd.go
+++ grabxkcd/grabxkcd.go
@@ -1,9 +1,21 @@
 package grabxkcd
 
+import (
+	"os"
+)
+
 type appEnv struct {
+}
+
+func (app *appEnv) fromArgs([]string) error {
+	return nil
 }
 
 func CLI(args []string) int {
 	var app appEnv
+	err := app.fromArgs(args)
+	if err != nil {
+		return 2
+	}
 	return 0
 }
